5366311f646ebd355a3c5be72399d516a4336154
Dragutu Maria <mariadragutu6@gmail.com>
2025-06-24 15:38:58 +0300

    home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter  as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter  as Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Work from './pages/Work';
@@ -8,7 +8,7 @@ import Contact from './pages/Contact';
 function App() {
   return (
       <><Header /><Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/work" element={<Work />} />
       <Route path="/contact" element={<Contact />} />
     </Routes></>

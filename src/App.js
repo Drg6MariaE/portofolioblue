5d4176cc02b52078c9695d13722c13e48f31e2b4
Dragutu Maria <mariadragutu6@gmail.com>
2025-06-24 15:13:33 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter  as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Work from './pages/Work';
@@ -7,14 +7,11 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+      <><Header /><Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/work" element={<Work />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes></>
   );
 }
 
